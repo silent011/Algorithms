eacd24e27f378066fe7cb494a28f09fa47299335
Silent-Era <junkyard@abv.bg>
2018-03-16 22:54:01 +0200

    inverting an array with recursion

---
+++ SortingNSearching/Recusrion/InvertingArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class InvertingArray<T>
+{
+    private static T[] result;
+    public static T[] Inverse(T[] arr)
+    {
+        result = new T[arr.Length];
+        Inverse(arr, 0);
+        return result;
+    }
+
+    private static void Inverse(T[] arr, int index)
+    {
+        if(index == arr.Length)
+        {
+            return;
+        }
+
+        Inverse(arr, index + 1);
+        result[index] = arr[arr.Length - 1 - index];
+    }
+}
+
---
+++ SortingNSearching/Recusrion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string[] arr = Console.ReadLine().Split(' ');
+
+            var result = InvertingArray<string>.Inverse(arr);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
