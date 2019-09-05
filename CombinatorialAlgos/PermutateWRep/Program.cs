d1c59e1c47091464907dd30180b3b6501b85116d
Silent-Era <junkyard@abv.bg>
2019-09-06 01:52:08 +0300

    variations with and without repetitions

--- CombinatorialAlgos/PermutateWRep/Program.cs
+++ CombinatorialAlgos/PermutateWRep/Program.cs
@@ -1,26 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string[] arr = Console.ReadLine().Split(' ');
+     
+            string[] arr = Console.ReadLine().Split(' ');
+            //Array.Sort(arr);
 
-        Array.Sort(arr);
+            //Perm.PermWRep(arr, 0, arr.Length - 1);
+            Perm.PermRepHash(arr, 0);
+       
+    }
 
-        PermWRep(0, arr.Length - 1);
-        void PermWRep(int start, int end)
+    static class Perm
+    {
+        static int count = 0;
+        public static void PermWRep(string[] arr, int start, int end)
         {
-            Console.WriteLine(string.Join(" ", arr));
-
+            Print(arr, count);
             for (int left = end - 1; left >= start; left--)
             {
                 for (int right = left + 1; right <= end; right++)
                 {
                     if(arr[left] != arr[right])
                     {
-                        Swap(left, right);
-                        PermWRep(left + 1, end);
+                        Swap(arr, left, right);
+                        PermWRep(arr, left + 1, end);
                     }
                 }
 
@@ -33,13 +40,41 @@ class Program
                 arr[end] = firstElement;
             }
         }
-        
-        void Swap(int i1, int i2)
+
+        public static void PermRepHash(string[] arr, int index)
+        {
+            if(index >= arr.Length)
+            {
+                count++;
+                Print(arr, count);
+                return;
+            }
+            HashSet<string> swapped = new HashSet<string>();
+            for (int i = index; i < arr.Length; i++)
+            {
+                if (!swapped.Contains(arr[i]))
+                {
+                    Swap(arr, index, i);
+                    PermRepHash(arr, index + 1);
+                    Swap(arr, index, i);
+                    swapped.Add(arr[i]);
+                }
+            }
+            
+        }
+
+        static void Swap(string[] arr, int i1, int i2)
         {
             string temp = arr[i1];
             arr[i1] = arr[i2];
             arr[i2] = temp;
         }
+
+        static void Print(string[] arr, int count)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
     }
 }
 
---
+++ CombinatorialAlgos/VariationsNoReps/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(' ');
+        int k = int.Parse(Console.ReadLine());
+
+        //Vars.VarsNoReps(arr, k);
+        //Vars.VarsWReps(arr, k);
+
+        Vars.VarsWRepsIterative(arr, k);
+    }
+
+    static class Vars
+    {
+        static bool[] used = new bool[0];
+        static int k = 0;
+        static string[] vars = new string[0];
+
+        public static void VarsNoReps(string[] arr, int limit)
+        {
+            used = new bool[arr.Length];
+            vars = new string[limit];
+            k = limit;
+
+            for (int i = 0; i < used.Length; i++)
+            {
+                used[i] = false;
+            }
+
+            getVarsNoReps(arr, 0);
+
+            //reset static variables
+            ResetVars();
+        }
+
+        private static void getVarsNoReps(string[] arr, int index)
+        {
+            if(index >= k)
+            {
+                Print(vars);
+                return;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    vars[index] = arr[i];
+                    getVarsNoReps(arr, index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+
+        public static void VarsWReps(string[] arr, int limit)
+        {
+            k = limit;
+            vars = new string[k];
+
+            getVarsWReps(arr, 0);
+        }
+
+        public static void VarsWRepsIterative(string[] arr, int limit)
+        {
+            int n = arr.Length;
+            int[] indexes = new int[limit];
+            string[] result = new string[limit];
+            while (true)
+            {
+                Print(result.Select((x, i) => x = arr[indexes[i]]).ToArray());
+
+                int index = limit - 1;
+                while (index >= 0 && indexes[index] == n - 1)
+                {
+                    index--;
+                }
+
+                if (index < 0) break;
+                indexes[index]++;
+                for (int i = index + 1; i < limit; i++)
+                {
+                    indexes[i] = 0;
+                }
+            }
+        }
+
+        private static void getVarsWReps(string[] arr, int index)
+        {
+            if(index >= k)
+            {
+                Print(vars);
+                return;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                vars[index] = arr[i];
+                getVarsWReps(arr, index + 1);
+            }
+        }
+
+        static void ResetVars()
+        {
+            used = new bool[0];
+            vars = new string[0];
+            k = 0;
+        }
+
+        static void Print(string[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
