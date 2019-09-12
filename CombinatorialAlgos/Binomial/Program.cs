3bbfaa8f567fa339797c3d7ebdbf2d3a2f9d7f84
Silent-Era <junkyard@abv.bg>
2019-09-12 04:16:48 +0300

    recursive algorithm for generation of combinations

---
+++ CombinatorialAlgos/Binomial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        int limit = int.Parse(Console.ReadLine());
+        decimal[,] memo = new decimal[count + 1, limit + 1];
+
+        Console.WriteLine(Binom(count, limit));
+
+
+        decimal Binom(int n, int k)
+        {
+            if (k > n) return 0;
+
+            if (k == 0 || k == n)
+            {
+                return 1;
+            }
+
+            if (memo[n, k] != 0)
+            {
+                return memo[n, k];
+            } 
+
+            decimal result = Binom(n - 1, k - 1) + Binom(n - 1, k);
+
+            memo[n, k] = result;
+
+            return result;
+        }
+    }
+}
+
---
+++ CombinatorialAlgos/Combinations/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputArr = Console.ReadLine().Split(' ');
+        int k = int.Parse(Console.ReadLine());
+
+        // Comb.CombsNoReps(inputArr, k);
+        Comb.CombsWReps(inputArr, k);
+    }
+
+    static class Comb
+    {
+        static string[] input = new string[0];
+        static int k = 0;
+        public static void CombsNoReps(string[] inputArr,int limit)
+        {
+            SetInternals(inputArr, limit);
+            string[] arr = new string[k];
+            CombsNoRepsRec(arr, 0, 0);
+
+            ResetInternals();
+        }
+
+        private static void SetInternals(string[] inputArr, int limit)
+        {
+            input = inputArr;
+            k = limit;
+        }
+
+        private static void CombsNoRepsRec(string[] arr, int start, int index)
+        {
+            if(index >= k)
+            {
+                Print(arr);
+                return;
+            }
+
+            for (int i = start; i < input.Length; i++)
+            {
+                arr[index] = input[i];
+                CombsNoRepsRec(arr, i + 1, index + 1);
+            }
+        }
+
+        public static void CombsWReps(string[] inputArr, int limit)
+        {
+            SetInternals(inputArr, limit);
+
+            string[] arr = new string[k];
+            CombsWRepsRec(arr, 0, 0);
+
+            ResetInternals();
+        }
+
+        private static void CombsWRepsRec(string[] arr, int index, int start)
+        {
+            if(index >= k)
+            {
+                Print(arr);
+                return;
+            }
+
+            for (int i = start; i < input.Length; i++)
+            {
+                arr[index] = input[i];
+                CombsWRepsRec(arr, index + 1, i);
+            }
+        }
+
+        static void ResetInternals()
+        {
+            input = new string[0];
+            k = 0;
+        }
+
+        static void Print(string[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
+
