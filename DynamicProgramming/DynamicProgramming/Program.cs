4c076229e141875c4c58dab3bf30d15ac08df121
Silent-Era <junkyard@abv.bg>
2019-09-27 22:32:07 +0300

    fibonacci dynamic programming

---
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] memo = new int[N + 1];
+
+        Fib(N);
+        Console.WriteLine(memo[N]);
+
+        int Fib(int n)
+        {
+            if (n <= 0) return memo[n] = 0;
+            if (n == 1) return memo[n] = 1;
+            if (memo[n] > 0) return memo[n];
+
+            return memo[n] = Fib(n - 1) + Fib(n - 2);
+        }
+
+    }
+}
+
