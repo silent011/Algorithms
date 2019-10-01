92f439191187098cd865cf3f95fbc592196cb826
Silent-Era <junkyard@abv.bg>
2019-10-01 15:50:26 +0300

    binomial coefficients using dynamic programming

---
+++ DynamicProgramming/Binomial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int K = int.Parse(Console.ReadLine());
+
+        long[,] memo = new long[N + 1, K + 1];
+
+        Console.WriteLine(GetBinomial(N, K));
+         
+        long GetBinomial(int n, int k)
+        {
+            if (k == 0) return 1;
+            if (n == 0) return 0;
+
+            if (memo[n, k] > 0) return memo[n, k];
+
+            return memo[n, k] = GetBinomial(n - 1, k -1)
+                + GetBinomial(n - 1, k);
+        }
+    }
+}
